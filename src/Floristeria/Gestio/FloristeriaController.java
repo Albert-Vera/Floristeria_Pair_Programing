6e86fc566d18cad76f2b4780ff07e214fae805be
Yohanna Mideros <mideros1@gmail.com>
2020-07-27 12:06:09 +0200

    Adiciono metodos para insertar una arbol y adicionar precio.

--- src/Floristeria/Gestio/FloristeriaController.java
+++ src/Floristeria/Gestio/FloristeriaController.java
@@ -3,6 +3,7 @@ package Floristeria.Gestio;
 
 import Floristeria.Model.Floristeria;
 import Floristeria.persistence.FloristeriaRepository;
+import Floristeria.Model.Arbre;
 
 public class FloristeriaController {
 
@@ -16,6 +17,12 @@ public class FloristeriaController {
 		Floristeria florist = new Floristeria(name, address, phone);
 		repository.setFlorist(florist);
 		//System.out.println(""+florist.getNom()+ " | "+ florist.getAdress()+ " | " +florist.getPhone());
+	}
+	
+	public void addTree(String name, double price, double height) throws Exception {
+		Arbre tree = new Arbre(name, price, height);
+		repository.addArticle(tree);
+	//	System.out.println(""+tree.getNom()+ " | "+tree.getPreu()+ " | "+ tree.getAltura());
 	}
 
 }
