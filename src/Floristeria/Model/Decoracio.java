8ecc3b7f94a9fe25f1216f215e37bc1ad734ab1e
Yohanna Mideros <mideros1@gmail.com>
2020-07-27 13:04:27 +0200

    Adiciono una variable para almacenar el valor seleccionado, y instancio variables de la super class

--- src/Floristeria/Model/Decoracio.java
+++ src/Floristeria/Model/Decoracio.java
@@ -1,7 +1,26 @@
 package Floristeria.Model;
 
-public class Decoracio extends Article{
-    public enum Decorat_Type{
-        FUSTA, PLASTIC
-    }
+public class Decoracio extends Article {
+
+	private String material;
+
+	public Decoracio() {
+	}
+
+	public Decoracio(String nom, double preu, String material) {
+		super(nom, preu);
+		this.material = material;
+	}
+
+	public enum Decorat_Type {
+		FUSTA, PLASTIC
+	}
+
+	public String getMaterial() {
+		return material;
+	}
+
+	public void setMaterial(String material) {
+		this.material = material;
+	}
 }
